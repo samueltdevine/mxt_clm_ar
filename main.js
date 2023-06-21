8c0789a8b1f91c5ba916906b51adb819feb24288
Sam DeVine <60078630+samueltdevine@users.noreply.github.com>
2023-06-21 21:53:02 +0000

    Update 3 files sync attempt

--- main.js
+++ main.js
@@ -23,12 +23,23 @@ const plane = new THREE.Mesh(geometry, material);
 geometry.translate(0, 0, 0);
 
 const geoYellow = new THREE.PlaneGeometry(7.0, 6.72);
+
 const videoYellow = document.getElementById("videoYellow");
-videoYellow.play();
 const textureYellow = new THREE.VideoTexture(videoYellow);
 textureYellow.format = THREE.RGBAFormat;
+
+const videoYellowAlpha = document.getElementById("videoYellowAlpha");
+
+const textureYellowAlpha = new THREE.VideoTexture(videoYellowAlpha);
+textureYellowAlpha.format = THREE.RGBAFormat;
+
+// videoYellow.muted = true;
+
+// videoYellowAlpha.muted = true;
+
 const materialYellow = new THREE.MeshBasicMaterial({
   map: textureYellow,
+  alphaMap: textureYellowAlpha,
   // color: 0x00ff00,
   transparent: true,
   opacity: 90,
@@ -39,17 +50,46 @@ geoYellow.rotateX(degToRad(45));
 geoYellow.scale(1 / 14, 1 / 14, 1 / 14);
 
 anchor.group.add(planeYellow);
-anchor.group.add(plane);
+// anchor.group.add(plane);
+videoYellowAlpha.play();
+videoYellow.play();
+
 const start = async () => {
   await mindarThree.start();
   renderer.setAnimationLoop(() => {
     renderer.render(scene, camera);
   });
 };
+
+var videos = document.querySelectorAll("video");
+
+// Create a promise to wait all videos to be loaded at the same time.
+// When all of the videos are ready, call resolve().
+var promise = new Promise(function (resolve) {
+  var loaded = 0;
+
+  videos.forEach(function (v) {
+    v.addEventListener("loadedmetadata", function () {
+      loaded++;
+
+      if (loaded === videos.length) {
+        resolve();
+      }
+    });
+  });
+});
+
+// Play all videos one by one only when all videos are ready to be played.
+promise.then(function () {
+  videos.forEach(function (v) {
+    v.play();
+  });
+});
 const startButton = document.querySelector("#startButton");
 startButton.addEventListener("click", () => {
   start();
 });
+
 stopButton.addEventListener("click", () => {
   mindarThree.stop();
   mindarThree.renderer.setAnimationLoop(null);
