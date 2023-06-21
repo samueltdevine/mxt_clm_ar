97f07cbea62d9a7c4c743b145873aaf873fa061f
Sam DeVine <60078630+samueltdevine@users.noreply.github.com>
2023-06-21 16:57:04 +0000

    Update 5 files

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
-import * as THREE from 'three';
-import {ARButton} from 'three/examples/jsm/webxr/ARButton'
+import * as THREE from "three";
+import { ARButton } from "three/examples/jsm/webxr/ARButton";
+import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
+
+const mindARThree = new MindARThree({
+	container: document.querySelector('#container'),
+	imageTargetSrc: 
+})
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.xr.enabled = true
-document.body.appendChild( renderer.domElement );
-const button = ARButton.createButton(renderer)
-document.body.appendChild(button)
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.xr.enabled = true;
+document.body.appendChild(renderer.domElement);
+const button = ARButton.createButton(renderer);
+document.body.appendChild(button);
 
-const geometry = new THREE.BoxGeometry( .1, .1, .1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
 animate();
