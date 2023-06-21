22d95abc49d39fba1a85967224a26bd1a4380592
Sam DeVine <60078630+samueltdevine@users.noreply.github.com>
2023-06-21 19:23:34 +0000

    testing mp4

--- main.js
+++ main.js
@@ -4,16 +4,41 @@ const mindarThree = new MindARThree({
   container: document.querySelector("#container"),
   imageTargetSrc: "/cover.mind",
 });
+
+function degToRad(degrees) {
+  var pi = Math.PI;
+  return degrees * (pi / 180);
+}
+
 const { renderer, scene, camera } = mindarThree;
 const anchor = mindarThree.addAnchor(0);
 const geometry = new THREE.PlaneGeometry(1, 0.55);
+
 const material = new THREE.MeshBasicMaterial({
-  color: 0x00ffff,
-  transparent: true,
-  opacity: 0.5,
+  color: 0x272727,
+  transparent: false,
+  opacity: 0.2,
 });
 const plane = new THREE.Mesh(geometry, material);
+geometry.translate(0, 0, 0);
 
+const geoYellow = new THREE.PlaneGeometry(7.0, 6.72);
+const videoYellow = document.getElementById("videoYellow");
+videoYellow.play();
+const textureYellow = new THREE.VideoTexture(videoYellow);
+textureYellow.format = THREE.RGBAFormat;
+const materialYellow = new THREE.MeshBasicMaterial({
+  map: textureYellow,
+  // color: 0x00ff00,
+  transparent: true,
+  opacity: 90,
+});
+const planeYellow = new THREE.Mesh(geoYellow, materialYellow);
+geoYellow.translate(-3, 2, 2);
+geoYellow.rotateX(degToRad(45));
+geoYellow.scale(1 / 14, 1 / 14, 1 / 14);
+
+anchor.group.add(planeYellow);
 anchor.group.add(plane);
 const start = async () => {
   await mindarThree.start();
